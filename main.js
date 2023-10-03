5f23a5e136685e09b6065028323b80c3b3f7047e
hamed <hamedmohamed9887@gmail.com>
2023-10-03 16:50:37 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,146 @@
+
+// let num1 =prompt(' number1');
+// let num2 =prompt(' number2');
+// let num3 =prompt(' number3');
+// let num4 =prompt(' number4');
+// let num5 =prompt(' number5');
+// let num6 =prompt(' number6');
+// let num7 =prompt(' number7');
+// let num8 =prompt(' number8');
+// let num9 =prompt(' number9');
+// let num10 =prompt(' number10');
+//    let sum = (+num1+ +num2+ +num3+ +num4+ +num5+ +num6+ +num7+ +num8+ +num9+ +num10)
+//     let avr = sum/10
+// document.write(avr);
+
+ 
+    
+// let hight =prompt('hight');
+// let weight =prompt(' weight');   
+//      let area = (1/2*weight*hight);
+//    document.write(area) ; 
+ 
+   
+
+
+
+//  let age =prompt('Your age by year'); 
+//    let life = (365*age)
+//    alert(life+'days');
+  
+
+
+
+// let number1 = prompt('First Number');
+// let number2 = prompt('second Number');
+//   let sum = (+number1 + +number2);
+//   document.write(sum);
+
+
+
+// let arr1 = prompt('arr1');
+// let arr2 = prompt('arr2');
+// let arr3 = prompt('arr3');
+// let arr4 = prompt('arr4');
+// let arr5 = prompt('arr5');
+// let arr6 = prompt('arr6');
+// let arr7 = prompt('arr7');
+// let arr8 = prompt('arr8');
+// let arr9 = prompt('arr9');
+// let arr10 = prompt('arr10');
+// let names = [arr1,arr2,arr3,arr4,arr5,arr6,arr7,arr8,arr9,arr10]
+// console.log(names[0])
+
+
+// let mynm =prompt('what your name?');
+// document.getElementById("h1_h1").innerHTML = mynm;
+
+
+
+
+// let hours = prompt('hours');
+//  second = (hours*60);
+//  alert(second+'second');
+
+
+
+// let num1 = prompt('first number');
+// let num2 = prompt('second number');
+// let num3 = prompt('third number');
+// let num4 = prompt('last number');
+//  sum = (+num1 + +num2 + +num3 + +num4);
+
+//  if(sum>350)
+//  {
+//     nam =('true')
+//    document.getElementById("h1_h1").innerHTML = nam; 
+//  }
+//  else
+//  {
+//     nam =('false');
+//    document.getElementById("h1_h1").innerHTML = nam; 
+//  }
+
+
+
+// let num =prompt('number 0-1 only')
+// if(num==0)
+// {
+//     document.write('true')
+// }
+// else if(num==1)
+// {
+//     document.write('false')
+// }
+// else
+// {
+//     document.write('please 0 and 1 only')
+// }
+
+
+
+
+// let number1 =prompt('first number');
+// let number2 =prompt('second number');
+// if(number1>number2)
+// {
+//     sum =(number2%number1)
+//     document.write(sum)
+// }
+// else if(number2>number1)
+// {
+//     sum =(number1%number2)
+//     document.write(sum)
+// }
+
+
+
+
+
+
+//  let number1 =prompt('first number');
+//  let number2 =prompt('second number');
+//  if(number1>number2)
+// {  
+//     document.write(number1)
+//  }
+//  else if(number2>number1)
+// {  
+//     document.write(number2)
+//  }
+//  else if(number2==number1)
+//  {  
+//     sum = number1==number2
+//      document.write(sum)
+//   }
+
+
+
+  
+//    document.write(typeof )
+
+
+
+// let date = new Date("");
+// alert(date)
+
